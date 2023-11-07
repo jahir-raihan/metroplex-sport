74485ffdc57c4eafc69d562ad997432bc32aaffe
jahir-raihan <raihan.joy10@gmail.com>
2023-11-07 18:54:40 +0600

    Implementing Class based approach for simplicity

--- Design-Diagram-Templates/templates/js/time-slots.js
+++ Design-Diagram-Templates/templates/js/time-slots.js
@@ -22,240 +22,228 @@ const change_arrow_and_border = (ele, id) => {
 
 
 
-
-
 // Selected time slots - payment method - team info
 
-let selected_slot_datas = {
-    //* Skeleton view 
-    // 'Date_SportType_CourtId_ContractDuration': {
-    //     date: 'Date',
-    //     court_id: 'courtId',
-    //     contract_duration: 'contract_duration',
-    //     sport_type: 'sport_type',
-    //     total_fare: 0,
-    //     time_slots_and_teams: {
-    //         slotid: {
-    //             time: 'time',
-    //             team1: 'team1',
-    //             team2: 'team2'
-    //         }
-    //     }
 
-    // }
-    // End Skeleton view
-}
 let count_selected_slots = 0;
-let current_date = null;
 let current_sport_type = null;
 let current_contract_duration = null;
 let current_fare = null;
 
-// Update current page data function
 
-function update_page_data(){
-    current_date = $('#current-date').val();
-    current_sport_type = $('#current-sport-type').val();
-    current_contract_duration = $('#current-contract-duration').val();
-    current_fare = $('#current-fare').val();
-}
-
-// End Update current page data function
-
-// Check slot availability
-
-function check_slot_avilability(slot){
-
-}
-
-// End Check slot availability
-
-
-// Check slot is already there
-
-
-function check_slot_is_already_there(data, slot){
-    if (slot.id in data){
-        return true;
-    }
-    return false;
-}
-
-// End Check slot is already there
-
-
-
-// Add slot data to selected slots datas dictionary
-
-function add_slot_data(data_id, slot){
-
-    let slot_parent = selected_slot_datas[data_id]
-
-    let already_there = check_slot_is_already_there(slot_parent.time_slots_and_teams, slot)
-
-    if (!already_there){
-        slot_parent.time_slots_and_teams[slot.id] = {
-            time: slot.getAttribute('data-time') + ' ' + slot.getAttribute('data-when'),
-            team1: '',
-            team2: ''
-        }
-        slot.disabled = true 
-        slot.classList.add('selected')
-
-        slot_parent.total_fare += current_fare;
-
-    }
-}
-// End Add slot data to selected slots datas dictionary
-
-
-// Update data listings 
-
-function update_data_listings(data_id, slot){
-    let slot_parent = selected_slot_datas[data_id]
-
-    $('#data-table-'+slot.getAttribute('data-court-id')).html(` <tr class="header-row"><td>DateTime</td><td>Fare</td><td>Action</td>
-                                                                </tr><tr id="data-table-row-B2"> <td></td></tr>`)
-
-    $('#team-details-container-'+slot_parent.court_id).html(`<div id="${slot_parent.court_id}-teams-data"></div>`)
-    
-    if (slot_parent.total_fare > 0){
-        $('#selections-and-team-details-'+slot.getAttribute('data-court-id')).removeClass('d-none-force');
-        
-        let date = slot_parent.formatted_date;
-
-        for (slot in slot_parent.time_slots_and_teams){
-
-            let cur_slot = slot_parent.time_slots_and_teams[slot]
-
-            let new_row = ` <tr id="data-row-${slot}">
-                        <td>${date} <span>${cur_slot.time}</span></td>
-                        <td>${slot_parent.fare_rate} TK</td>
-                        <td onclick="remove_slot('${slot}')"><i class="fas fa-circle-xmark"></i></td>
-                    </tr>`
-            $('#data-table-row-'+slot_parent.court_id).before(new_row);
-
-            let team_data = `<div class="team-name-input">
-                                <input type="text" placeholder="type here" value="${cur_slot.team1}"  data-team="1" data-slot-id="${slot}" data-court-id="${slot_parent.court_id}" id="${slot}-team-1">
-                                <p>VS</p>
-                                <input type="text" placeholder="type here" value="${cur_slot.team2}" data-team="2" data-slot-id="${slot}" data-court-id="${slot_parent.court_id}" id="${slot}-team-2">
-                            </div>`
-            
-            $('#'+slot_parent.court_id+'-teams-data').before(team_data)
-
-        }
-        
-        
-       
-    }
-
-}
-
-// End Update data listings
-
-
-// Update summary data
-
-function update_summary_data(){
-    
-    let summary_total = $('#summary-total')
-
-    $('#summary-table').html(`<tr class="table-head"><td>DateTime</td><td>Amount</td></tr>
-                              <tr id="summary-table-row"> <td></td></tr>`)
-    let summary_table = $('#summary-table-row')
-
-    for (data_id in selected_slot_datas){
-        let slot_parent = selected_slot_datas[data_id]
-        let slots = selected_slot_datas[data_id].time_slots_and_teams
-        let total = 0;
-
-        for (slot in slots){
-            let row = `<tr>
-                            <td>
-                                <span>${slot_parent.formatted_date}</span>
-                                <span>${slots[slot].time}</span>
-                            </td>
-                            <td>
-                                ${slot_parent.fare_rate} TK
-                            </td>
-                        </tr>`
-            total += Number(slot_parent.fare_rate)
-            summary_table.before(row);
-            summary_total.html(`${total}`)
-
-        }
-        
-    }
-}
-
-// End Update summary data
-
-
-// Add slot function
-
-function add_slot(slot){
-
-    // Check for availablity
-
-    // let availablity = check_slot_avilability(slot);
-    // if (! availablity.status){
-    //     slot.disabled = true;
-    //     slot.classList.add(availablity.classname)
-    //     return;
-    // }
-
-    // End Check for availabilty
-
-    // Update current page data 
-
-    update_page_data()
-
-    // End Update current page data 
-
-
-    // Add or append data
-
-    var data_id = `${current_date}_${current_sport_type}_${slot.getAttribute('data-court-id')}_${current_contract_duration}`
-    if (data_id in selected_slot_datas){
-        
-        add_slot_data(data_id, slot)
-
-    } else {
-
-        selected_slot_datas[data_id] = {
-            date: current_date,
-            formatted_date: $('#formatted-date').val(),
-            court_id: slot.getAttribute('data-court-id'),
-            contract_duration: current_contract_duration,
-            sport_type: current_sport_type,
-            total_fare: current_fare,
-            fare_rate : current_fare,
-            time_slots_and_teams: {
-            }
-        
-        }
-        selected_slot_datas[data_id].time_slots_and_teams[slot.id] = {
-            time: slot.getAttribute('data-time') + ' ' + slot.getAttribute('data-when'),
-            team1: '',
-            team2: ''
-        }
-
-        slot.disabled = true;
-        slot.classList.add('selected')
-
-    }
-
-    // End Add or append data
-
-
-    // Update data listings and summary
-
-    update_data_listings(data_id, slot);
-    update_summary_data();
-
-    // End Update data listings and summary
-}
-
-// End Add slot function
 
 // End Selected time slots - payment method - team info
+
+
+// Class based approach
+
+
+class Renderer{
+
+    constructor(){
+        this.storage = null;
+        this.current_date = new Date();
+        this.searched_date = $('#current-date').val();
+        this.contract_duration = $('#current-contract-duration').val();
+        this.fare = $('#current-fare').val();
+        this.sport_type = $('#current-sport-type').val();
+        this.formatted_date = $('#formatted-date').val();
+    }
+
+    get_ls_data(){
+        if ('slots' in localStorage){
+            this.storage = JSON.parse(localStorage.getItem('slots'))
+        } else {
+            localStorage.setItem('slots', '{}')
+            this.storage = {}
+        }
+    }
+    
+    update_ls_data(){
+        /* Update localstorage data along with data in backend for expired slots */
+
+        localStorage.setItem('slots', JSON.stringify(this.storage))
+    }
+
+    render_slots(){
+    
+    }
+
+    render_summary(){
+        let summary_total = $('#summary-total')
+
+        $('#summary-table').html = ''
+        $('#summary-table').html(`<tr class="table-head"><td>DateTime</td><td>Amount</td></tr>
+                                <tr id="summary-table-row"> <td></td></tr>`)
+        let summary_table = $('#summary-table-row')
+        
+        let total = 0;
+        for (let data_id in this.storage){
+            let slot_parent = this.storage[data_id]
+            let slots = this.storage[data_id].time_slots_and_teams
+            
+            total += Number(slot_parent.total_fare)
+            for (let slot in slots){
+                this.render_selected_slot(data_id, slot_parent.court_id)
+                let row = `<tr>
+                                <td>
+                                    <span>${slot_parent.formatted_date}</span>
+                                    <span>${slots[slot].time}</span>
+                                </td>
+                                <td>
+                                    ${slot_parent.fare_rate} TK
+                                </td>
+                            </tr>`
+                
+                summary_table.before(row);
+                
+
+            }
+            
+        }
+        summary_total.html(`${total}`)
+    }
+
+    render_selected_slot(key, court_id){
+
+        /* Method to render a selected slot data in summary table */
+        // Yeah it's a little mess, but it's worth it.
+
+        let slot_parent = this.storage[key]
+
+        $('#data-table-'+court_id).html(` <tr class="header-row"><td>DateTime</td><td>Fare</td><td>Action</td>
+                                                                    </tr><tr id="data-table-row-B2"> <td></td></tr>`)
+
+        $('#team-details-container-'+slot_parent.court_id).html(`<div id="${slot_parent.court_id}-teams-data"></div>`)
+        
+        if (slot_parent.total_fare > 0){
+            $('#selections-and-team-details-'+court_id).removeClass('d-none-force');
+            
+            let date = slot_parent.formatted_date;
+
+            for (let slot in slot_parent.time_slots_and_teams){
+
+                let cur_slot = slot_parent.time_slots_and_teams[slot]
+                $('#'+slot).addClass('selected')
+
+                let new_row = ` <tr id="data-row-${slot}">
+                            <td>${date} <span>${cur_slot.time} ${cur_slot.when}</span></td>
+                            <td>${slot_parent.fare_rate} TK</td>
+                            <td onclick="remove_slot('${slot}')"><i class="fas fa-circle-xmark"></i></td>
+                        </tr>`
+                $('#data-table-row-'+slot_parent.court_id).before(new_row);
+
+                let team_data = `<div class="team-name-input">
+                                    <input type="text" placeholder="type here" value="${cur_slot.team1}"  data-team="1" data-slot-id="${slot}" data-court-id="${slot_parent.court_id}" id="${slot}-team-1">
+                                    <p>VS</p>
+                                    <input type="text" placeholder="type here" value="${cur_slot.team2}" data-team="2" data-slot-id="${slot}" data-court-id="${slot_parent.court_id}" id="${slot}-team-2">
+                                </div>`
+                
+                $('#'+slot_parent.court_id+'-teams-data').before(team_data)
+
+            }
+            
+            
+        
+        }
+    }
+
+    is_expired(slot){
+    
+    }
+
+    remove_slot(slot){
+    
+    }
+
+    get_formatted_time(){
+        let time = `${this.current_date.getHours()}:${this.current_date.getMinutes()}:${this.current_date.getSeconds()}`
+        let date = `${this.current_date.getDate()}/${this.current_date.getMonth()}/${this.current_date.getFullYear()}`
+
+        return `${date} ${time}`
+    }
+
+    add_slot(slot){
+
+        /* Add slot data to local storage with all kind of logical checks, eg: Availablity,
+            Eligibility and Limit etc. And mark that slot as selected in the database as well.
+        */
+
+        let key = `${this.searched_date}_${this.sport_type}_${slot.getAttribute('data-court-id')}_${this.contract_duration}`
+        
+        let data = null;
+
+        // If key is not in local storage then create a new skelteton for that key.
+        if (key in this.storage){
+            data = this.storage[key]
+        } else {
+            data = {
+                date: this.searched_date,
+                formatted_date: this.formatted_date,
+                court_id: slot.getAttribute('data-court-id'),
+                contract_duration: this.contract_duration,
+                sport_type: this.sport_type,
+                total_fare: 0,
+                fare_rate: this.fare,
+                time_slots_and_teams: {}
+
+            }
+        }
+
+        // If the slot is available and does not exist already in localstorage then add it.
+        let is_avialable = this.check_availablity(slot, data);
+
+        if (is_avialable && !this.slot_exist(slot, data, key)){
+        
+            data['time_slots_and_teams'][`${slot.getAttribute('id')}`] = {
+                add_time: this.get_formatted_time(),
+                time: slot.getAttribute('data-time'),
+                when: slot.getAttribute('data-when'),
+                team1: 'Red',
+                team2: 'Blue'
+            }
+            data['total_fare'] += Number(this.fare)
+            $(`#${slot.getAttribute('id')}`).addClass('selected');
+        }
+
+        this.storage[key] = data;
+
+        // Update everything.
+        this.update_ls_data();
+        this.render_summary();
+
+
+    }
+
+    slot_exist(slot, data, key){
+        /* Check, if the selected slot is already in localstorage */
+        if (key in this.storage &&
+            slot.getAttribute('id') in this.storage[key]['time_slots_and_teams']) {
+            console.log("slot exists")
+            return true;
+        }
+        return false;
+    }
+
+    check_availablity(slot, data){
+
+        /* Send request to backend for availabilty check and return true if available else 
+           False 
+        */
+       return true;
+
+    }
+
+    request_mark_selected(slot){
+    
+    }
+
+
+}
+
+let renderer = new Renderer();
+renderer.get_ls_data();
+renderer.update_ls_data();
+
+
+renderer.render_summary()
