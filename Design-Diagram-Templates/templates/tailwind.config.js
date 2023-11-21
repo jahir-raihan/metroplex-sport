78c88ee96e04d83f6c83a6f13f5795943d29ae12
jahir-raihan <raihan.joy10@gmail.com>
2023-11-21 20:47:27 +0600

    Added Registration page

---
+++ Design-Diagram-Templates/templates/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./html/*.{html,js}'],
+  theme: {
+    width:{
+      'login-width': '360px',
+      'full': '100%',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
