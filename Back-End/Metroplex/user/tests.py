1cd22559d8068b377591dce6aa2d9f8ccfbe7f5c
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-02-11 23:33:10 +0600

    Writing test skeleton for abstracted base user model and it's view functionalities

--- Back-End/Metroplex/user/tests.py
+++ Back-End/Metroplex/user/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class UserTests(TestCase):
+
+    """
+    Test cases abstracted user model
+    """
+
+    def test_user_register(self):
+
+        """
+        Test user register view
+        """
+        pass
+
+    def test_user_login(self):
+
+        """
+        Test user login view
+        """
+        pass
+
+    def test_user_logout_view(self):
+
+        """
+        Test user logout view
+        """
+        pass
+
+    def test_duplicated_account(self):
+
+        """
+        Test for duplicate account creation
+        """
+        pass
+
+    def test_login_while_logged_in(self):
+
+        """
+        Test to check if a user is able to access and
+        login while logged in.
+        """
+        pass
+
+    def test_logout_while_not_signed_in(self):
+
+        """
+        Test if a user is able to access and logout while
+        not signed in.
+        """
+        pass
