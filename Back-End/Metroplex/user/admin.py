6d8f1babdf67c88d3ab1a929e94f2a6b1e4a8475
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-03-09 13:51:00 +0600

    Customized admin panel for better visibility

--- Back-End/Metroplex/user/admin.py
+++ Back-End/Metroplex/user/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+User = get_user_model()
+
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'name')}),
+        ('Permissions', {'fields': (
+            'is_active',
+            'is_staff',
+            'is_admin',
+            'is_superuser'
+
+        )}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('email', 'password1', 'password2')
+            }
+        ),
+    )
+
+    list_display = ('email', 'name', 'id', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
