f5531efd3a6442174a7eba9a4a71bb657a7376df
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-03-10 23:15:23 +0600

    Added Home page with views, urls and template

--- Back-End/Metroplex/booking/views.py
+++ Back-End/Metroplex/booking/views.py
@@ -1,3 +1,26 @@
+# Imports
+
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+# End Imports
+
+
+class HomeView(View):
+
+    """
+    Home view of metro-plex website
+    """
+    template_name = 'search.html'
+
+    def get(self, request):
+
+        """
+        Get method for rendering search page with some banners
+        :param request:
+        :return:
+        """
+
+        context = {}
+
+        return render(request, self.template_name, context)
