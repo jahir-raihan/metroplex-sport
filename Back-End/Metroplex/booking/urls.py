78f4278f461a0ca64c4ffcfe42a9ecd1b24518c8
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-06-06 22:07:09 +0600

    Adding search view

--- Back-End/Metroplex/booking/urls.py
+++ Back-End/Metroplex/booking/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, SearchView
 
 urlpatterns = [
-    path("", HomeView.as_view(), name='home')
+    path("", HomeView.as_view(), name='home'),
+    path('search', SearchView.as_view(), name='search')
 ]
--- Back-End/Metroplex/booking/views.py
+++ Back-End/Metroplex/booking/views.py
@@ -1,7 +1,10 @@
 # Imports
 
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Court, TimeSlot
+from datetime import datetime
+from django.db.models import Q
 
 # End Imports
 
@@ -19,6 +22,10 @@ class HomeView(View):
         Get method for rendering search page with some banners
         :param request:
         """
+
+        data = request.GET
+        if data.get('date'):
+            return redirect(f'/search?date={data.get('date')}&sport-type={data.get('sport-type')}')
 
         context = {}
 
@@ -38,8 +45,30 @@ class SearchView(View):
         """
         View after search is being made
         :param request:
-        :return:
+        :return courts&courts.timeslots:
         """
-        context = {}
+
+        data = request.GET
+        date = data.get('date')
+        sport_type = data.get('sport-type')
+        contract_type = data.get('contract_type')  # This is not required for now at all.
+
+        date = datetime.strptime(date, '%Y-%m-%d').date()
+
+        # Filter courts based on sport_type and date
+        courts = Court.objects.filter(
+            Q(created_at=date) & Q(court_name=sport_type)
+        ).prefetch_related('timeslot_set')
+
+        # Create context with courts and their timeslots
+        context = {
+            'courts': [
+                {
+                    'court': court,
+                    'timeslots': court.timeslot_set.all()
+                }
+                for court in courts
+            ]
+        }
 
         return render(request, self.template_name, context)
